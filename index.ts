23efce1351790979406b26b37f48fee42791debc
anzerr <thoron174+anzerr@gmail.com>
2022-10-26 15:14:26 +0200

    chore: missing version dump

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 
 import 'reflect-metadata';
 
-export * from './src/server';
 import Controller from './src/controller';
 import ServerController from './src/server/controller';
 
 export {Controller, ServerController};
 export * from './src/request';
+export * from './src/server';
